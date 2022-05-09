faf5154006e79b676fac5d98c39c2cb43b1d28b2
Mohd Nisab Alam <76525761+nisabmohd@users.noreply.github.com>
2022-05-09 15:11:13 +0530

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -18,7 +18,7 @@ export const Header = () => {
             <div style={{ display: 'flex', flexDirection: 'row',}} className="connectpages">
                 {/* <a href="/" style={{ fontSize: '15px', margin: '0 9px',fontWeight:'bold',letterSpacing:'1.8px',cursor:'pointer',textDecoration: 'none', color: 'white'}} class="glitch">
                     about</a> */}
-                <a style={{ textDecoration: 'none', color: 'white'}} href="https://drive.google.com/file/d/1AM5S54xfcepNgkmKDi3CGBppAKhr3BOY/view"><p style={{ fontSize: '15px', margin: '0 9px',fontWeight:'bold',letterSpacing:'1.8px'  }} class="glitch">
+                <a style={{ textDecoration: 'none', color: 'white'}} href="https://firebasestorage.googleapis.com/v0/b/upload-pics-e599e.appspot.com/o/pdf%2FnisabResume.pdf?alt=media&token=af4bb111-3d5f-4f48-a159-08adfdb930df"><p style={{ fontSize: '15px', margin: '0 9px',fontWeight:'bold',letterSpacing:'1.8px'  }} class="glitch">
                     resume</p>
                 </a>
                 <a style={{ textDecoration: 'none', color: 'white',fontWeight:'lighter',letterSpacing:'1.8px' }} href="http://www.github.com/nisabmohd">
